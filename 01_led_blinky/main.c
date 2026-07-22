78fb0a0f5023069930d351ce94f6e447a1578654
tangchunling230 <tangchunling@druid.tech>
2026-07-22 14:50:20 +0800

    Remodified according to coding standards

--- 01_led_blinky/main.c
+++ 01_led_blinky/main.c
@@ -3,7 +3,7 @@
 #include "nrf_delay.h"
 #include "nrf_gpio.h"
 
-#define LED_PIN    NRF_GPIO_PIN_MAP(1, 15)  //定义LED引脚：P1.15
+#define LED_PIN    NRF_GPIO_PIN_MAP(1, 15) //定义LED引脚：P1.15
 int main(void) {
   nrf_gpio_cfg_output(LED_PIN); // 1. 将 P1.15 引脚配置为输出模式
   nrf_gpio_pin_set(LED_PIN); // 2. 默认让引脚输出高电平（在官方 DK 板上，高电平会使 LED 熄灭）
